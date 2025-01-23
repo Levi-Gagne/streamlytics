bc9d922dfa1dd93f8b1613da2976bc24cb4cb957
Levi Gagne <67981022+Levi-Gagne@users.noreply.github.com>
2025-01-22 23:14:14 -0600

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -4,11 +4,17 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
 
-# Spotify Configuration (from Streamlit Secrets)
-SPOTIFY_CLIENT_ID = st.secrets["spotify"]["client_id"]
-SPOTIFY_CLIENT_SECRET = st.secrets["spotify"]["client_secret"]
-SPOTIFY_REDIRECT_URI = st.secrets["spotify"]["redirect_url"]
-SPOTIFY_SCOPE = st.secrets["spotify"]["scope"]
+# Spotify app configuration
+SPOTIFY_CLIENT_ID     = "1dcbd7d4fafb480ab60d84c309ad5626"
+SPOTIFY_CLIENT_SECRET = "49a0cae0cf834b1f84d6ac1090cec485"
+SPOTIFY_REDIRECT_URI  = "http://localhost:8080"
+
+SPOTIFY_SCOPE = (
+    "user-read-email user-read-private user-library-read user-library-modify "
+    "user-read-playback-state user-modify-playback-state user-read-currently-playing "
+    "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public "
+    "user-read-recently-played user-top-read user-follow-read user-follow-modify"
+)
 
 def authenticate_spotify():
     """
